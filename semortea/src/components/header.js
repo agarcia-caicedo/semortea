bffa6807335986df4742b92d3cd732818906346a
Ana Garcia <agarcia-caicedo@myseneca.ca>
2020-01-10 18:50:56 -0500

    customized header

--- semortea/src/components/header.js
+++ semortea/src/components/header.js
@@ -2,37 +2,60 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import image from "../images/teatwit.png"
+import icon from "../../public/icons/sm.png"
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
       backgroundImage:  `url(${image})`,
       marginBottom: `1.45rem`,
-      backgroundSize: `contain`,
-      backgroundPosition: `center`,
+      backgroundSize: `fill`,
+      backgroundPosition: `left top`,
+      backgroundRepeat: `no-repeat`,
+      overflow: `hidden`,
+      backgroundAttachment: `fixed`,
     }}
   >
     <div
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        minHeight: 300,
-        padding: `1.45rem 1.0875rem`,
-        borderRadius: `0 0 85% 85%`,
+        maxWidth: `100%`,
+        minHeight: `35vh`,
+        textAlign: `center`,
+
+        borderStyle: `solid`,
+        borderWidth: `0.01rem`,
+        borderColor: `black`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h1
+      style={{
+        margin: 0,
+        }}>
         <Link
           to="/"
           style={{
             color: `white`,
             textDecoration: `none`,
             fontFamily: `Amatic SC`,
+            fontSize:`4rem`,
+            backgroundColor: `black`,
+            borderRadius: `10%`,
           }}
         >
           {siteTitle}
         </Link>
       </h1>
+
+      <img src={icon} style={{
+        margin: 0,
+        borderRadius:`50%`,
+        borderStyle: `solid`,
+        borderWidth: `0.20rem`,
+        borderColor: `white`,
+        width: `12%`,
+        height: `auto`,
+        backgroundColor: `black`,
+      }} alt="avatar"/>
     </div>
   </header>
 )
